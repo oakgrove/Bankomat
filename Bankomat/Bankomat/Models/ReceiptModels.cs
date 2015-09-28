8911d7c753929ab2aae715141c637970eed146de
Johan Järvenhag <j.jarvenhag@gmail.com>
2015-09-28 13:54:55 +0200

    RecepitModels added

---
+++ Bankomat/Bankomat/Models/ReceiptModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bankomat.Models
+{
+    public class ReceiptModels
+    {
+        public int ID { get; set; }
+        public double Length { get; set; } = 14;
+        public bool Active { get; set; }
+    }
+}
