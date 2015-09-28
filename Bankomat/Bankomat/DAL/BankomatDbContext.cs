044ee18ac257d3ecd6136a5d79037f315f632fc6
Sebastian Mebius <sebastian.mebius@gmail.com>
2015-09-28 13:19:43 +0200

    Added remove prulaizing

---
+++ Bankomat/Bankomat/DAL/BankomatDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Bankomat.DAL
+{
+    public class BankomatDbContext : DbContext
+    {
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
