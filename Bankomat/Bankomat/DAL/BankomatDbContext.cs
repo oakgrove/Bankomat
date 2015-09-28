67f531c31585ce0b242a04fb3ae6fa18b1d7bdbe
Sebastian Mebius <sebastian.mebius@gmail.com>
2015-09-28 13:50:49 +0200

    Money Added

--- Bankomat/Bankomat/DAL/BankomatDbContext.cs
+++ Bankomat/Bankomat/DAL/BankomatDbContext.cs
@@ -4,11 +4,14 @@ using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
+using Bankomat.Models;
 
 namespace Bankomat.DAL
 {
     public class BankomatDbContext : DbContext
     {
+
+        public DbSet<MoneyModel> Money { get; set; }
 
         public BankomatDbContext() : base("Bankomat")
         {
