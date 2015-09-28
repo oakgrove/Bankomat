3181a22324fffde34eb51105e5230e23a8ffc3d2
Susanna Stenson <susannastenson@gmail.com>
2015-09-28 14:41:43 +0200

    Added ForeignKey

--- Bankomat/Bankomat/Models/AccountModel.cs
+++ Bankomat/Bankomat/Models/AccountModel.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Web;
 using Bankomat.Models;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bankomat.Models
 {
     public class AccountModel
     {
-       
+        
         public int Id { get; set; }
-        public int UserID { get; set; }
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
         public int AccountNumber { get; set; }
         public string Name { get; set; }
         public double Balance { get; set; }
--- Bankomat/Bankomat/Models/TransactionModel.cs
+++ Bankomat/Bankomat/Models/TransactionModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,7 @@ namespace Bankomat.Models
         public int Id { get; set; }
         public DateTime TransactionTime { get; set; }
         public string TransactionType { get; set; }
+        [ForeignKey("AccountId")]
         public int AccountId { get; set; }
         public double Balance { get; set; }
         public double Amount { get; set; }
