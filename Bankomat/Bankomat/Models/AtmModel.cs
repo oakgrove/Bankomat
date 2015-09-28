d162f2308ece678e49e07079b9d8b46403f47a6e
Linnea Eriksson <erikssonlinneam@gmail.com>
2015-09-28 13:55:11 +0200

    ATM added

---
+++ Bankomat/Bankomat/Models/AtmModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bankomat.Models
+{ // ID, LIST pengar LIST receipt
+    public class AtmModel
+    {   
+        public int MyProperty { get; set; }
+    }
+}
